b84f54eba6abd80fba6889fce8a818985861048d
mrrolbot <mrrolbot@mrrolbot.com>
2022-06-17 09:34:53 -0700

    update search bar error and mobile components

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -62,8 +62,8 @@ const Search = () => {
             <img src={coin.image} alt="" />
             <p>{coin.symbol.toUpperCase()}</p>
         </div>
-        <p>${coin.current_price.toLocaleString()}</p>
-        <p>{coin.price_change_percentage_24h.toFixed(2)}%</p>
+        <p>${coin.current_price}</p>
+        <p>{coin.price_change_percentage_24h}%</p>
         <p className='hide-mobile'>${coin.total_volume.toLocaleString()}</p>
         <p className='hide-mobile'>${coin.market_cap.toLocaleString()}</p>
       </div>
--- src/routes/Coin.jsx
+++ src/routes/Coin.jsx
@@ -2,7 +2,6 @@ import React, {useState, useEffect} from 'react'
 import { useParams } from 'react-router-dom'
 import axios from 'axios'
 import DOMPurify from 'dompurify' 
-
 import './Coin.css'
 
 const Coin = () => {
@@ -46,20 +45,20 @@ const Coin = () => {
                 <table>
                     <thead>
                         <tr>
-                            <th>1h</th>
+                            <th className='hide-mobile'>1h</th>
                             <th>24h</th>
                             <th>7d</th>
-                            <th>14d</th>
+                            <th className='hide-mobile'>14d</th>
                             <th>30d</th>
                             <th>1yr</th>
                         </tr>
                     </thead>
                     <tbody>
                         <tr>
-                            <td>{coin.market_data?.price_change_percentage_1h_in_currency ? <p>{coin.market_data.price_change_percentage_1h_in_currency.usd}%</p> : null}</td>
+                            <td className='hide-mobile'>{coin.market_data?.price_change_percentage_1h_in_currency ? <p>{coin.market_data.price_change_percentage_1h_in_currency.usd}%</p> : null}</td>
                             <td>{coin.market_data?.price_change_percentage_24h_in_currency ? <p>{coin.market_data.price_change_percentage_24h_in_currency.usd}%</p> : null}</td>
                             <td>{coin.market_data?.price_change_percentage_7d_in_currency ? <p>{coin.market_data.price_change_percentage_7d_in_currency.usd}%</p> : null}</td>
-                            <td>{coin.market_data?.price_change_percentage_14d_in_currency ? <p>{coin.market_data.price_change_percentage_14d_in_currency.usd}%</p> : null}</td>
+                            <td className='hide-mobile'>{coin.market_data?.price_change_percentage_14d_in_currency ? <p>{coin.market_data.price_change_percentage_14d_in_currency.usd}%</p> : null}</td>
                             <td>{coin.market_data?.price_change_percentage_30d_in_currency ? <p>{coin.market_data.price_change_percentage_30d_in_currency.usd}%</p> : null}</td>
                             <td>{coin.market_data?.price_change_percentage_1y_in_currency ? <p>{coin.market_data.price_change_percentage_1y_in_currency.usd}%</p> : null}</td>
                         </tr>
